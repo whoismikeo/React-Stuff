d7424af128dfe406e635fcacadf19274db55c654
whoismikeo <whoismikeo@gmail.com>
2019-11-25 23:34:56 -0800

    Put header elements into a single function

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -1,22 +1,34 @@
-// const title = 'My first React element!';
 
-const desc = 'I just learned how to create a heckin React node and render it to the DOM.';
+//~~~~~~~~Made this into a function below~~~~~~~~~
+// const desc = 'I just learned how to create a heckin React node and render it to the DOM.';
 
-const myTitleID = 'main-title'
+// const myTitleID = 'main-title'
 
-const name = 'Mike';
+// const name = 'Mike';
 
-const header = (
-    <header>
-    {/* This is a comment, my dude. */}
-       <h1 id={myTitleID}>{ name }'s first React element</h1> 
-       <p className="herpderp">{ desc }</p>
-    </header>
-);
+// const header = (
+//     <header>
+//     {/* This is a comment, my dude. */}
+//        <h1 id={myTitleID}>{ name }'s first React element</h1> 
+//        <p className="herpderp">{ desc }</p>
+//     </header>
+// );
+
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
+
+
+
 
 
 ReactDOM.render(
-    header,
+    <Header />,
     document.getElementById('root')
 );
 
