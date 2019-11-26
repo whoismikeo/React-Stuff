b5a5f57445ecc6f5e4e06e55e2a07429bef3b533
whoismikeo <whoismikeo@gmail.com>
2019-11-25 23:57:48 -0800

    Parent and child components, as yis!

--- 2 - Add React to a Project/scoreboard/app.js
+++ 2 - Add React to a Project/scoreboard/app.js
@@ -14,28 +14,58 @@
 //     </header>
 // );
 
-//---------Next, converting to arrow function-------
-// function Header() {
-//     return (
+//-----Header function as an arrow function-------
+// const Header = () => (
 //         <header>
 //             <h1>Scoreboard</h1>
 //             <span className="stats">Players: 1</span>
 //         </header>
 //     );
-// }
 
-function Header = () => (
+const Header = () => {
+    return (
         <header>
             <h1>Scoreboard</h1>
             <span className="stats">Players: 1</span>
         </header>
     );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
 
 
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Mike
+            </span>
+            <Counter />
+        </div>
+    );
+}
 
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+
+            {/* Players list */}
+            <Player />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    <Header />,
+    <App />,
     document.getElementById('root')
 );
 
